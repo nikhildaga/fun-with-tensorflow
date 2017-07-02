db1fa50cd44bb91237f42695b57c607cfadde4fa
nikhil daga <nikhil.daga.bitsian@gmail.com>
2017-07-02 01:34:45 -0700

    manuall fix loss_model

---
+++ manually_fix_loss_model.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+
+x = tf.placeholder(tf.float32)
+
+linear_model = W * x + b
+
+sess = tf.Session()
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+print(sess.run(linear_model, {x:[1,2,3,4]}))
+
+y = tf.placeholder(tf.float32)
+
+squared_deltas = tf.square(linear_model - y)
+
+loss = tf.reduce_sum(squared_deltas)
+
+print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+
+fixW = tf.assign(W, [-1.])
+fixb = tf.assign(b, [1.])
+sess.run([fixW, fixb])
+
+print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
