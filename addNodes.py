4a37ea52669c6d6105191088084ef15c2ee472a9
nikhil daga <nikhil.daga.bitsian@gmail.com>
2017-07-01 23:20:09 -0700

    add nodes

--- addNodes.py
+++ addNodes.py
@@ -3,8 +3,9 @@ import tensorflow as tf
 node1 = tf.constant(3.0, dtype=tf.float32)
 node2 = tf.constant(4.0) # also tf.float32 implicitly
 
-print(node1, node2)
-
 sess = tf.Session()
 
-print(sess.run([node1, node2]))
+node3 = tf.add(node1, node2)
+
+print("node3: ", node3)
+print("sess.run(node3): ",sess.run(node3))
